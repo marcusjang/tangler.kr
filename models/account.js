becc9c0b5bb92600b1d96ec2b62e3423a2a8e9a0
Marcus Jang <makerpopo@gmail.com>
2018-03-10 03:47:19 +0900

    Add username charset and length validation
    Also fix count preserve routine

--- models/account.js
+++ models/account.js
@@ -5,7 +5,15 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const counter = require('./counter');
 
 let Account = new Schema({
-	username: { type: String, required: true, unique: true, index: true, lowercase: true },
+	username: { type: String, required: true, unique: true, lowercase: true, validate: {
+		validator: (v) => {
+			let length = /^[a-zA-Z0-9._@-]{4,32}$/.test(v);
+			let chars = !(/[._@-]{2,}/.test(v));
+			console.log (length, chars);
+			return (length && chars);
+		},
+		message: '{PATH} can only consist of 4 to 32 characters of alphabets, numbers, and non-consecutive at signs, underscores, dashes, or dots'
+	} },
 	password: { type: String, select: false },
 	level: { type: Number, default: 1 },
 	id: { type: Number, unique: true, index: true },
@@ -14,7 +22,7 @@ let Account = new Schema({
 
 Account.plugin(passportLocalMongoose);
 
-Account.pre('save', async next => {
+Account.pre('save', async function(next) {
 	let doc = this;
 	try {
 		let count = await counter.findByIdAndUpdate(
